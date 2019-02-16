075134d320f637217771fcd5f8f6e0e7017528c3
siva8910 <sivan.sundar@gmail.com>
2019-02-16 11:37:13 +0530

    Text detection

--- app/src/main/java/com/seeme/MainActivity.java
+++ app/src/main/java/com/seeme/MainActivity.java
@@ -1,19 +1,35 @@
 package com.seeme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.camerakit.CameraKitView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "Captured Image LOG : ";
+
 
     @BindView(R.id.camera)
     CameraKitView camera;
@@ -62,6 +78,50 @@ public class MainActivity extends AppCompatActivity {
         camera.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+
+    private void detetText(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+
+        Task<FirebaseVisionText> result = textRecognizer.processImage(image)
+                .addOnCompleteListener(new OnCompleteListener<FirebaseVisionText>() {
+                    @Override
+                    public void onComplete(@NonNull Task<FirebaseVisionText> task) {
+                        String result = task.getResult().getText();
+                        showAlert(result);
+                        Log.d(TAG, "onComplete: \n" + result);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
+    private void showAlert(String result) {
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.setMessage(result + "\n");
+        alertDialog.show();
+
+    }
+
     @OnClick(R.id.capture)
     public void onViewClicked() {
 
@@ -69,10 +129,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onImage(CameraKitView cameraKitView, byte[] bytes) {
              //   Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                Toast.makeText(MainActivity.this, "gfhfhkfkh", Toast.LENGTH_SHORT).show();
 
-                Intent i = new Intent(MainActivity.this, CapturedImage.class);
-                i.putExtra("byteArray", bytes);
-                startActivity(i);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                detetText(bitmap);
 
             }
         });
