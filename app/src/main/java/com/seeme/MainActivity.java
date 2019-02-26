6263dec74affb199461f2f65642d981b1a0bd26d
Sivan <sivan.sundar@gmail.com>
2019-02-26 18:56:57 +0530

    GSign In implemented successfully. Moving on to OCR

--- app/src/main/java/com/seeme/MainActivity.java
+++ app/src/main/java/com/seeme/MainActivity.java
@@ -1,36 +1,71 @@
+/*
+ *
+ *  * Copyright (c) 2018 Sivan Chakravarthy
+ *  *  *
+ *  *  * MIT License
+ *  *  *
+ *  *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  *  * of this software and associated documentation files (the "Software"), to deal
+ *  *  * in the Software without restriction, including without limitation the rights
+ *  *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  *  * copies of the Software, and to permit persons to whom the Software is
+ *  *  * furnished to do so, subject to the following conditions:
+ *  *  *
+ *  *  * The above notice shall be included in all
+ *  *  * copies or substantial portions of the Software.
+ *  *  *
+ *  *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  *  * SOFTWARE.
+ *  *  *
+ *  *
+ *
+ */
+
 package com.seeme;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.camerakit.CameraKitView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
-import com.seeme.Fragments.LoginFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     //  1:58AM. Next batch of work listed below
     //  Setup Google Sign in Auth
@@ -38,17 +73,15 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
     //  Formulate effective means to process data from camera and store.
 
     private static final String TAG = "Captured Image LOG : ";
+    private static final int RC_SIGN_IN = 1;
+    @BindView(R.id.mGSignIn_btn)
+    SignInButton mGSignInBtn;
+
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
-    @BindView(R.id.camera)
-    CameraKitView camera;
-    @BindView(R.id.capture)
-    FloatingActionButton capture;
-    @BindView(R.id.mainFrame)
-    FrameLayout mainFrame;
-
-
-    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,48 +89,122 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        loginFragment = new LoginFragment();
-        goToLoginFragment(loginFragment);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(MainActivity.this, FirstScreen.class));
+                }
+
+            }
+        };
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.clientID))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                        Toast.makeText(MainActivity.this, "Connection failed. Couldn't connect to SeeMe", Toast.LENGTH_LONG).show();
+                        Log.d(TAG, "onConnectionFailed: " + connectionResult.getErrorMessage());
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
     }
 
-    private void goToLoginFragment(LoginFragment loginFragment) {
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, loginFragment);
-        fragmentTransaction.commit();
+
+
+
+
+    //           Log.w(TAG, "Google sign in failed", e);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success : "+ task.getResult().getUser().getUid());
+                            startActivity(new Intent(MainActivity.this, FirstScreen.class));
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Login Failed. Couldn't log into your Boltraz account", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        camera.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(MainActivity.this, FirstScreen.class));
+
+            Toast.makeText(this, "Logged in as " + currentUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        camera.onResume();
     }
 
     @Override
     protected void onPause() {
-        camera.onPause();
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        camera.onStop();
         super.onStop();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        camera.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
 
 
     private void detetText(Bitmap bitmap) {
@@ -143,6 +250,12 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
 
     }
 
+    @OnClick(R.id.mGSignIn_btn)
+    public void onViewClicked() {
+
+        signIn();
+    }
+
  /*   @OnClick(R.id.capture)
     public void onViewClicked() {
 
@@ -159,11 +272,6 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnF
         });
 
     }*/
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 
 
 }
